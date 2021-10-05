3c96dcb3e1b65b1252a54301b1eca4a3a0aeb546
ReoNaBear <adamsimprove1108@gmail.com>
2021-10-05 14:44:14 +0800

    feat:project init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+//const routes = require('./routes')
+//require('./config/mongoose')
+const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+//app.use(routes)
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
